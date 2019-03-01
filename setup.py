116efee1065e8c39388aa8c771b1a8aa81126156
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2019-03-01 12:59:03 +0300

    Попытался исправить manifest и setup

--- setup.py
+++ setup.py
@@ -1,14 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
 from setuptools import setup, find_packages
+from setuptools.command.install import install as st_install
+
+
+class install(st_install):
+    def _post_install(self, lib_dir):
+        packages = ('edw', 'email_auth', 'social_extra')
+        backend_dir = os.path.join(lib_dir, 'backend')
+        if os.path.exists(backend_dir):
+            for package in packages:
+                src_dir = os.path.join(backend_dir, package)
+                dst_dir = os.path.join(lib_dir, package)
+                if os.path.exists(dst_dir):
+                    shutil.rmtree(dst_dir)
+                shutil.copytree(src_dir, dst_dir, symlinks=True)
+            if os.path.exists(backend_dir):
+                shutil.rmtree(backend_dir)
+
+    def run(self):
+        st_install.run(self)
+        self.execute(self._post_install, (self.install_lib,),
+                     msg="Running post install task")
+
 
 setup(
     name='django-edw-fluent',
-    version='1.0.0',
+    version='1.0.1',
     description='Package for connect Django EDW and Django Fluent Pages.',
     author='Infolabs LLC',
     author_email='team@infolabs.ru',
     url='https://github.com/infolabs/django-edw-fluent',
     packages=find_packages(),
     zip_safe=False,
+    cmdclass={'install': install},
     package_data={
         'edw_fluent': [
             'locale/*/LC_MESSAGES/*',
