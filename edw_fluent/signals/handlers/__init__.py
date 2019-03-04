403cda8ab3f0a93b23fb05fbc7fc5e8a763329b1
ARTEM YAROVOY <yarovoy@excentrics.ru>
2019-03-04 14:07:49 +0300

    Добавлена инвалидация кеша страниц Simple Page

--- edw_fluent/signals/handlers/__init__.py
+++ edw_fluent/signals/handlers/__init__.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+from django.db.utils import ProgrammingError
+
+try:
+    from edw_fluent.signals.handlers import (
+        page_layout,
+        template,
+        entity,
+        data_mart,
+        term,
+        simple_page
+    )
+except (AttributeError, ProgrammingError) as e:
+    # initial migrations hack
+    print("*** INITIAL MIGRATIONS HACK ***")
+    print e.args
