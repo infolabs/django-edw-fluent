97008e9c950accb750d023b597465df04ddbf5b1
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2020-02-26 16:46:39 +0300

    NR-283 Add js and form template to the admin

--- edw_fluent/admin/page.py
+++ edw_fluent/admin/page.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.utils import six
 from edw_fluent.models.page import SimplePage
 from fluent_pages.pagetypes.fluentpage.admin import FluentPageAdmin
 
 
 @admin.register(SimplePage)
 class SimplePageAdmin(FluentPageAdmin):
-    pass
+    class Media:
+        js = ("edw_fluent/page/fluent_layouts.js",) if six.PY3 else ("fluent_pages/fluentpage/fluent_layouts.js",)
+
+    change_form_template = [
+        "edw_fluent/admin/change_form.html",
+    ]
