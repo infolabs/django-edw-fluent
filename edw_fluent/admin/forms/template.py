c6b19b5e7dbce1f38d1e6c5b746c03ee78b196ed
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2020-06-04 15:51:54 +0300

    MM-1165 Fix template form validation, fix translation

--- edw_fluent/admin/forms/template.py
+++ edw_fluent/admin/forms/template.py
@@ -21,7 +21,7 @@ class BaseTemplateForm(OriginalEntityAdminForm):
         """
         fields = '__all__'
         widgets = {
-          'template': PageBuilderWidget,
+            'template': PageBuilderWidget,
         }
 
 
@@ -33,14 +33,13 @@ class TemplateForm(BaseTemplateForm):
         'has_view_layout_error': _("The view layout of the publication isn't defined"),
     }
 
-    def clean(self):
+    def clean_terms(self):
         """
         RUS: Словарь проверенных и нормализованных данных формы шаблона.
         Вызывает ошибку валидации формы, если нет
         списка терминов представлений разметок страниц и терминов в очищенных данных.
         """
-        cleaned_data = super(TemplateForm, self).clean()
-
-        # view layout
-        if not (set([v.id for (k, v) in get_views_layouts().items()]) & set([x.id for x in cleaned_data.get("terms")])):
+        terms = self.cleaned_data.get("terms")
+        if not (set([v.id for (k, v) in get_views_layouts().items()]) & set([x.id for x in terms])):
             raise forms.ValidationError(self.messages['has_view_layout_error'])
+        return terms
