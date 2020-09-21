a10276d12c90f3d6012677c4da817174528970f9
beloslav <velislav1329@gmail.com>
2020-09-21 13:51:16 +0300

    OSK-49 добавил кол-во просмотров у публикации

--- edw_fluent/views/publication.py
+++ edw_fluent/views/publication.py
@@ -32,7 +32,8 @@ class PublicationViewSet(EntityViewSet):
                 cookie_name = "_seen_{}".format(pk)
                 if cookie_name not in request.COOKIES:
                     response.set_cookie(cookie_name, '1')
-                    PublicationBase.objects.filter(id=pk).update(statistic=F('statistic') + 1)
+                    self.get_object().get_real_instance().__class__().objects.filter(id=pk).update(
+                        statistic=F('statistic') + 1)
 
             for characteristic in response.data.get('characteristics', []):
                 if characteristic['path'] == 'entity/publication_wrapper/redirect-url':
