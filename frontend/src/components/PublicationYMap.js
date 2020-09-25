0566f351af717a7059c1c10a0d022ffeee78987f
Roman Uskov <romanuskov94@gmail.com>
2020-09-25 11:20:01 +0300

    OSK-48 подправил компонент для карт публикации для вывода допинформации (дата и просмотры) и немного стили

--- frontend/src/components/PublicationYMap.js
+++ frontend/src/components/PublicationYMap.js
@@ -21,13 +21,22 @@ class PublicationYMap extends YMapInner {
         exTags = this.exTags(marks),
         messageId = description ? description.id : null;
 
+    const created_at = new Date(item.extra.created_at);
+    const statistic = item.extra.statistic;
+    console.log(Object.keys(item.extra).length)
+
     let annotations = {};
     if (item.extra) {
       for (const [key, val] of Object.entries(item.extra)) {
-        if (val instanceof Object && 'name' in val && 'value' in val)
+        if (key === 'created_at') {
+          annotations[key] = new Date(val);
+        } else if (key === 'statistic') {
           annotations[key] = val;
+        }
+
       }
     }
+    console.log(annotations);
 
     return (
       <div className="ex-map-info"
@@ -46,22 +55,18 @@ class PublicationYMap extends YMapInner {
                   {child.values.join("; ")}
                 </li>
             )}
-            {Object.keys(annotations).length !== 0 &&
-              <li className="annotation">
-                {Object.keys(annotations).map(
-                  (key, i) =>
-                    <div key={i}>
-                      <strong>{annotations[key].name}:&nbsp;</strong>
-                      {annotations[key].value instanceof Array ?
-                        annotations[key].value.map((val, key) => <span key={key}>{val};&nbsp;</span>)
-                      :
-                        <span key={key}>{annotations[key].value}</span>
-                      }
-                    </div>
-                )}
-              </li>
-            }
           </ul>
+          {Object.keys(annotations).length &&
+            <div className="publication-extra">
+              <hr className="publication-extra__hr"/>
+              <strong><i className="fa fa-calendar" aria-hidden="true"></i></strong>&nbsp;
+              {annotations.created_at.getDate() > 9 ? created_at.getDate() : '0' + created_at.getDate()}.
+              {annotations.created_at.getMonth() + 1 > 9 ? created_at.getMonth() + 1 : '0' + (created_at.getMonth() + 1)}.
+              {annotations.created_at.getFullYear()}
+              <strong className="second"><i className="fa fa-eye" aria-hidden="true"></i></strong>&nbsp;
+              {annotations.statistic}
+            </div>
+          }
           {exTags}
         </div>
       </div>
