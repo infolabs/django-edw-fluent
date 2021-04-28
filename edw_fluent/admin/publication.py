9337052cedd95081128f6341f5d1bf7b98d6dc2b
aaartyomsss <aaartyomsss@gmail.com>
2021-04-28 16:31:39 +0300

    ALL-894 Добавил проверку прав при добавлении публикации в админке

--- edw_fluent/admin/publication.py
+++ edw_fluent/admin/publication.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-
+from django.core.exceptions import PermissionDenied
+from django.conf import settings
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
+from fluent_contents.models import Placeholder
 from fluent_contents.admin import PlaceholderFieldAdmin
 from fluent_contents.plugins.rawhtml.models import RawHtmlItem
 
@@ -153,15 +155,25 @@ class BasePublicationAdmin(PlaceholderFieldAdmin, EntityChildModelAdmin):
         """
         Добавляет в форму модель администратора
         """
+        form = super(BasePublicationAdmin, self).get_form(request, obj, **kwargs)
         if obj:
-            self.publication_id_for_formfield = obj.id
-        return super(BasePublicationAdmin, self).get_form(request, obj, **kwargs)
+            form.publication_id_for_formfield = obj.id
+        form.request = request
+        return form
+    
+    def save_model(self, request, obj, form, change):
+        user = request.user
+        app_label = settings.EDW_APP_LABEL
+        add_entity_relations_permission = "{}.add_entityrelation".format(app_label)
+        if not user.has_perm(add_entity_relations_permission):
+            error_message = _("You miss following permissions: %s") % add_entity_relations_permission
+            raise PermissionDenied(error_message)
+        super(BasePublicationAdmin, self).save_model(request, obj, form, change)
 
     def save_related(self, request, form, formsets, change):
         """
         Сохраняет связанные модели объектов, определяет placeholder и создает текстовый блок
         """
-
         # remove emoji from html items
         for fs in formsets:
             for f in fs:
@@ -171,11 +183,25 @@ class BasePublicationAdmin(PlaceholderFieldAdmin, EntityChildModelAdmin):
                 if isinstance(obj, RawHtmlItem):
                     obj.html = remove_emoji(obj.html)
 
+        app_label = settings.EDW_APP_LABEL
+        required_permissions = ["fluent_contents.add_placeholder", "fluent_contents.change_placeholder",
+                          "{}.add_blockitem".format(app_label), "{}.change_blockitem".format(app_label)]
+        missing_permissions = []
+
+        for perm in required_permissions:
+            if not request.user.has_perm(perm):
+                missing_permissions.append(perm)
+
+        if missing_permissions:
+            error_message = _("You miss following permissions: %s") % " ".join(missing_permissions)
+            raise PermissionDenied(error_message)
+
         super(BasePublicationAdmin, self).save_related(request, form, formsets, change)
-
         entity_id = int(form.instance.id)
-
-        placeholder = form.instance.get_or_create_placeholder()
+        try:
+            placeholder = form.instance.get_or_create_placeholder()
+        except Placeholder.MultipleObjectsReturned:
+            placeholder = Placeholder.objects.filter(parent_id=entity_id).first()
 
         if not BlockItem.objects.filter(placeholder_id=placeholder.id).exists():
             BlockItem.objects.create(
